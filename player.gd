91e254cebe56a44dfd46373e8cbdde3d05ea167a
Holayn <kwwong1597@gmail.com>
2021-03-28 16:39:37 -0400

    add sword player equip

--- player.gd
+++ player.gd
@@ -1,10 +1,15 @@
 extends KinematicBody2D
 
+var sword = preload("res://sword.tscn")
+
 var move_dir = Vector2.ZERO
 var velocity = Vector2.ZERO
 var speed = 100
 const ACCEL = 600
 const FRICTION = 500
+
+func _ready():
+	equip_weapon(sword)
 
 func _process(delta):
 	controls_process(delta)
@@ -22,3 +27,9 @@ func controls_process(delta):
 	move_dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	move_dir.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	move_dir = move_dir.normalized()
+
+func equip_weapon(weapon):
+	var weapon_instance = weapon.instance()
+	$ysort.add_child(weapon_instance)
+	weapon_instance.position = $ysort/weapon_slot.position
+	
